59c72e8a023e853ae25a8ba651ca289092fd0665
Panduranga <panduranga1797@gmail.com>
2025-10-01 10:15:14 +0530

    Initial commit

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI);
+    const conn = await mongoose.connect('mongodb+srv://panduranga1797_db_user:jlDRkGrjDbkaTy0E@test.hhe5wmp.mongodb.net/omnitrix?retryWrites=true&w=majority');
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (err) {
     console.error('MongoDB connection failed:', err.message);
